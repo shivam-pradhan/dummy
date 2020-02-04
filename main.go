dbd94e1e53a0d99da4fa141f801a2227486ab36e
shivam-pradhan <shivam.anuj.pradhan@gmail.com>
2020-02-04 16:02:28 +0530

    wffwefwe

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
-package dummy
+package main
 
 import (
 	"fmt"
 	"strconv"
 )
+
+func main(){
+	fmt.Println("dummy main...")
+	Add(1,2)
+}
 
 func Add(a int, b int){
 	fmt.Print( "A and B sum is : " + strconv.Itoa(a+b))
